05ac3d6a5e96e1bad8c5b865f2d841b3d30fab8f
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-03-24 13:33:29 +0100

    rendering with swift calls from objc

--- OGLEngine/Renderer.swift
+++ OGLEngine/Renderer.swift
@@ -11,6 +11,61 @@ import GLKit
 
 class Renderer : NSObject {
     
+    
+    class func bind(renderable: Renderable) {
+        glBindVertexArrayOES(renderable.vao.vaoGLName)
+        glEnableVertexAttribArray(GLuint(VboIndex.Positions.rawValue))
+        glEnableVertexAttribArray(GLuint(VboIndex.Texels.rawValue))
+        glEnableVertexAttribArray(GLuint(VboIndex.Normals.rawValue))
+        glEnableVertexAttribArray(GLuint(VboIndex.Tangents.rawValue))
+        glEnableVertexAttribArray(GLuint(VboIndex.Bitangents.rawValue))
+    }
+    
+    class func unbind(renderable: Renderable) {
+        glDisableVertexAttribArray(GLuint(VboIndex.Positions.rawValue))
+        glDisableVertexAttribArray(GLuint(VboIndex.Texels.rawValue))
+        glDisableVertexAttribArray(GLuint(VboIndex.Normals.rawValue))
+        glDisableVertexAttribArray(GLuint(VboIndex.Tangents.rawValue))
+        glDisableVertexAttribArray(GLuint(VboIndex.Bitangents.rawValue))
+        
+        glBindVertexArrayOES(0);
+    }
+    
+    class func bindTexture(renderable: Renderable, normalMap: Texture) {
+        glActiveTexture(GLenum(GL_TEXTURE0));
+        glBindTexture(GLenum(GL_TEXTURE_2D), renderable.texture.glName)
+        glActiveTexture(GLenum(GL_TEXTURE1));
+        glBindTexture(GLenum(GL_TEXTURE_2D), normalMap.glName);
+        glUniform1i(Program.ProgramUniformTexture, 0);
+        glUniform1i(Program.ProgramUniformNormalMap, 1);
+    }
+    
+    class func passMatrices(renderable: Renderable, camera: Camera) {
+        var modelMatrix = renderable.geometryModel.modelMatrix()
+        var viewMatrix = camera.viewMatrix()
+        var projectionMatrix = camera.projectionMatrix()
+        
+        withUnsafePointer(&modelMatrix, {
+            glUniformMatrix4fv(Program.ProgramUniformModelMatrix, 1, 0, UnsafePointer($0))
+        })
+        withUnsafePointer(&viewMatrix, {
+            glUniformMatrix4fv(Program.ProgramUniformViewMatrix, 1, 0, UnsafePointer($0))
+        })
+        withUnsafePointer(&projectionMatrix, {
+            glUniformMatrix4fv(Program.ProgramUniformProjectionMatrix, 1, 0, UnsafePointer($0))
+        })
+        
+        var normalMatrix = GLKMatrix3InvertAndTranspose(GLKMatrix4GetMatrix3(renderable.geometryModel.modelMatrix()), nil);
+        withUnsafePointer(&normalMatrix, {
+            glUniformMatrix3fv(Program.ProgramUniformNormalMatrix, 1, 0, UnsafePointer($0))
+        })
+
+    }
+    
+    class func draw(renderable: Renderable) {
+        glDrawElements(GLenum(GL_TRIANGLES), GLsizei(renderable.vao.vertexCount), GLenum(GL_UNSIGNED_INT), nil);
+    }
+    
     class func render(renderables: [Renderable], camera: Camera, normalMap: Texture) {
         for renderable in renderables {
             // Bind vao
@@ -47,7 +102,7 @@ class Renderer : NSObject {
             
             var normalMatrix = GLKMatrix3InvertAndTranspose(GLKMatrix4GetMatrix3(renderable.geometryModel.modelMatrix()), nil);
             withUnsafePointer(&normalMatrix, {
-                glUniformMatrix4fv(Program.ProgramUniformNormalMatrix, 1, 0, UnsafePointer($0))
+                glUniformMatrix3fv(Program.ProgramUniformNormalMatrix, 1, 0, UnsafePointer($0))
             })
             
             // Draw
