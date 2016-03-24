3ca45e52a8c2f74da71b7c3ccd595b2d632a1902
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-03-24 17:51:55 +0100

    strange type alias behavior

--- OGLEngine/OBJLoader.h
+++ OGLEngine/OBJLoader.h
@@ -1,62 +1,62 @@
+////
+////  OBJLoader.h
+////  jj
+////
+////  Created by Maciej Chmielewski on 16.02.2016.
+////  Copyright © 2016 AppUnite. All rights reserved.
+////
 //
-//  OBJLoader.h
-//  jj
+//#import <Foundation/Foundation.h>
+//#import <OpenGLES/ES2/glext.h>
+//#import "OGLEngine-Swift.h"
 //
-//  Created by Maciej Chmielewski on 16.02.2016.
-//  Copyright © 2016 AppUnite. All rights reserved.
+//@class OBJLoadingResult, StrideData, StrideCollection;
 //
-
-#import <Foundation/Foundation.h>
-#import <OpenGLES/ES2/glext.h>
-#import "OGLEngine-Swift.h"
-
-@class OBJLoadingResult, StrideData, StrideCollection;
-
-@interface OBJLoader : NSObject
-
-+ (OBJ *)objFromFileNamed:(NSString *)fileName;
-+ (OBJLoadingResult *)loadOBJNamed:(NSString *)name;
-+ (StrideCollection *)strideDataFromOBJLoadingResult:(OBJLoadingResult *)objLoadingResult;
-+ (OBJ *)objFromStrideCollection:(StrideCollection *)strideCollection objLoadingResult:(OBJLoadingResult *)objLoadingResult;
-
-@end
-
-
-@interface OBJLoadingResult : NSObject
-
-@property (nonatomic, strong, readonly) OBJInfo *objInfo;
-@property (nonatomic, assign, readonly) GLfloat **positions;
-@property (nonatomic, assign, readonly) GLfloat **texels;
-@property (nonatomic, assign, readonly) GLfloat **normals;
-@property (nonatomic, assign, readonly) GLint **faces;
-
-@end
-
-
-@interface StrideData : NSObject
-
-@property (nonatomic, assign) NSUInteger positionIndex;
-@property (nonatomic, assign) NSUInteger texelIndex;
-@property (nonatomic, assign) NSUInteger normalIndex;
-
-@property (nonatomic, assign) GLfloat *positions;
-@property (nonatomic, assign) GLfloat *texels;
-@property (nonatomic, assign) GLfloat *normals;
-
-- (instancetype)initWithPositionIndex:(NSUInteger)positionIndex
-                           texelIndex:(NSUInteger)texelIndex
-                          normalIndex:(NSUInteger)normalIndex
-                            positions:(GLfloat **)positions
-                               texels:(GLfloat **)texels
-                              normals:(GLfloat **)normals;
-@end
-
-
-@interface StrideCollection : NSObject
-
-- (void)addObject:(StrideData *)stride;
-
-@property (nonatomic, strong) NSMutableArray<StrideData *> *array;
-@property (nonatomic, strong) NSMutableArray<NSNumber *> *indices;
-
-@end
+//@interface OBJLoader : NSObject
+//
+//+ (OBJ *)objFromFileNamed:(NSString *)fileName;
+//+ (OBJLoadingResult *)loadOBJNamed:(NSString *)name;
+//+ (StrideCollection *)strideDataFromOBJLoadingResult:(OBJLoadingResult *)objLoadingResult;
+//+ (OBJ *)objFromStrideCollection:(StrideCollection *)strideCollection objLoadingResult:(OBJLoadingResult *)objLoadingResult;
+//
+//@end
+//
+//
+//@interface OBJLoadingResult : NSObject
+//
+//@property (nonatomic, strong, readonly) OBJInfo *objInfo;
+//@property (nonatomic, assign, readonly) GLfloat **positions;
+//@property (nonatomic, assign, readonly) GLfloat **texels;
+//@property (nonatomic, assign, readonly) GLfloat **normals;
+//@property (nonatomic, assign, readonly) GLint **faces;
+//
+//@end
+//
+//
+//@interface StrideData : NSObject
+//
+//@property (nonatomic, assign) NSUInteger positionIndex;
+//@property (nonatomic, assign) NSUInteger texelIndex;
+//@property (nonatomic, assign) NSUInteger normalIndex;
+//
+//@property (nonatomic, assign) GLfloat *positions;
+//@property (nonatomic, assign) GLfloat *texels;
+//@property (nonatomic, assign) GLfloat *normals;
+//
+//- (instancetype)initWithPositionIndex:(NSUInteger)positionIndex
+//                           texelIndex:(NSUInteger)texelIndex
+//                          normalIndex:(NSUInteger)normalIndex
+//                            positions:(GLfloat **)positions
+//                               texels:(GLfloat **)texels
+//                              normals:(GLfloat **)normals;
+//@end
+//
+//
+//@interface StrideCollection : NSObject
+//
+//- (void)addObject:(StrideData *)stride;
+//
+//@property (nonatomic, strong) NSMutableArray<StrideData *> *array;
+//@property (nonatomic, strong) NSMutableArray<NSNumber *> *indices;
+//
+//@end
--- OGLEngine/OBJLoader.swift
+++ OGLEngine/OBJLoader.swift
@@ -8,10 +8,61 @@
 
 import Foundation
 
-class OBJInfo : NSObject {
-    var vertices : Int = 0
-    var positions : Int = 0
-    var texels : Int = 0
-    var normals : Int = 0
-    var faces : Int = 0
+class OBJLoader : NSObject {
+    
+    class func objFromFileNamed(fileName: String) -> OBJ {
+        let filePath: String = NSBundle.mainBundle().pathForResource(fileName, ofType: "obj")!
+        
+        var payload = ""
+        do {
+            try payload = String(contentsOfFile: filePath)
+        } catch _ {
+        }
+        let lines: [String] = payload.componentsSeparatedByString("\n")
+        
+        var positions: [(x: Float, y: Float, z: Float)] = []
+        var texels: [(u: Float, v: Float)] = []
+        var normals: [(x: Float, y: Float, z: Float)] = []
+        typealias Vertex = (p: Int, t: Int, n: Int)
+        typealias Face = (v1: Vertex, v2: Vertex, v3: Vertex)
+        var faces: [Face] = []
+        
+        let stringToVec3 = { (string: String) -> (Float, Float, Float) in
+            let c = string.componentsSeparatedByString(" ")
+            return (Float(c[1])!, Float(c[2])!, Float(c[3])!)
+        }
+        let stringToVec2 = { (string: String) -> (Float, Float) in
+            let c = string.componentsSeparatedByString(" ")
+            return (Float(c[1])!, Float(c[2])!)
+        }
+        
+        let stringToStrideIndices = { (string: String) -> Vertex in
+            let c = string.componentsSeparatedByString("/")
+            return (Int(c[0])!, Int(c[1])!, Int(c[2])!)
+        }
+        let stringToFace = { (string: String) -> Face in
+            let c = string.componentsSeparatedByString(" ")
+            return (stringToStrideIndices(c[1]), stringToStrideIndices(c[2]), stringToStrideIndices(c[3]))
+        }
+        
+        
+        for line in lines {
+            if line.startIndex.distanceTo(line.endIndex) < 2 {
+                continue
+            }
+            switch line.substringToIndex(line.startIndex.advancedBy(2)) {
+            case "v ": positions.append(stringToVec3(line))
+            case "vt": texels.append(stringToVec2(line))
+            case "vn": normals.append(stringToVec3(line))
+            case "f ": faces.append(stringToFace(line))
+            default: break
+            }
+        }
+        
+        
+        
+        
+        
+        return OBJ()
+    }
 }
