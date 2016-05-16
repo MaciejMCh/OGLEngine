ccfd8c03a0959153aeb19d9d3e6c1a997a5f4ae7
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-05-16 20:50:20 +0200

    object in blend file has reflective flag property

---
+++ OGLEngine/ReflectiveSurfaceProgram.swift
@@ -0,0 +1,9 @@
+//
+//  ReflectiveSurfaceProgram.swift
+//  OGLEngine
+//
+//  Created by Maciej Chmielewski on 16.05.2016.
+//  Copyright © 2016 Maciej Chmielewski. All rights reserved.
+//
+
+import Foundation
