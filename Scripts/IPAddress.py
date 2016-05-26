7a77c6931921aafdf33d8ceeb2b9443f1407318d
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-05-26 20:39:45 +0200

    or not

--- Scripts/IPAddress.py
+++ Scripts/IPAddress.py
@@ -1,5 +1,5 @@
-#import socket
-#address = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1][0]
-#file_ = open('../OGLEngine/OGLEngine/OtherSources/IPAddress', 'w')
-#file_.write(address)
-#file_.close()
+import socket
+address = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1][0]
+file_ = open('../OGLEngine/OGLEngine/OtherSources/IPAddress', 'w')
+file_.write(address)
+file_.close()
