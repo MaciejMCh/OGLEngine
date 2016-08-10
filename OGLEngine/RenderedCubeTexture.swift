9f056d6b048252616cae585e80a3444d54c032da
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-08-10 17:27:04 +0200

    attachments

--- OGLEngine/RenderedCubeTexture.swift
+++ OGLEngine/RenderedCubeTexture.swift
@@ -53,7 +53,23 @@ class RenderedCubeTexture: CubeTexture {
     
     func bind(frameBuffer: UnsafeMutablePointer<GLuint>, textureSide: CubeTextureSide) {
         glBindFramebuffer(GLenum(GL_FRAMEBUFFER), frameBuffer.memory);
+        
+        let buffs: [GLenum] = [GLenum(GL_COLOR_ATTACHMENT0), GLenum(GL_COLOR_ATTACHMENT1)]
+        glDrawBuffers(2, buffs)
+        
+        glBindTexture(GLenum(GL_TEXTURE_CUBE_MAP), self.glName);
         glFramebufferTexture2D(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT0), textureSide.glEnum(), self.glName, 0);
+        
+        var sideTextureGlName = GLuint()
+        glGenTextures(1, &sideTextureGlName)
+        glBindTexture(GLenum(GL_TEXTURE_2D), sideTextureGlName);
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GL_CLAMP_TO_EDGE);
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_T), GL_CLAMP_TO_EDGE);
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_NEAREST);
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MAG_FILTER), GL_NEAREST);
+        glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GL_RGBA, w, h, 0, GLenum(GL_RGBA), GLenum(GL_UNSIGNED_BYTE), nil);
+        glFramebufferTexture2D(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT1), GLenum(GL_TEXTURE_2D), sideTextureGlName, 0)
+        self.sideTextures.append(CubeSideTexture(glName: sideTextureGlName, side: textureSide))
         
         var depthbuffer: GLuint = 0;
         glGenRenderbuffers(1, &depthbuffer);
