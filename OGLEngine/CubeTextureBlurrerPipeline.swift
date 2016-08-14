f9a4668cf66899bae9dd337f164251d97a946544
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-08-14 10:30:33 +0200

    before implementing int array uniform

--- OGLEngine/CubeTextureBlurrerPipeline.swift
+++ OGLEngine/CubeTextureBlurrerPipeline.swift
@@ -21,25 +21,19 @@ extension DefaultPipelines {
         let texel = Variable<GLSLVec2>(name: "texel")
         
         fragmentScope ✍ texel ⬅ vTexel
+        
+        // zoom
         let zoom: Float = 2
         fragmentScope ✍ texel ⬅ (texel * Primitive(value: zoom))
         fragmentScope ✍ texel ⬅ (texel - Primitive(value: GLKVector2Make(1.0 / zoom, 1.0 / zoom)))
         
         
-//        fragmentScope ✍ texelX ⬅ texel.>"x"
-//        fragmentScope ✍ texelY ⬅ texel.>"y"
-        
-        let wallIndex = Variable<GLSLInt>(name: "wallIndex")
         let transformationIndex = Variable<GLSLInt>(name: "transformationIndex")
-        let uIndex1 = Variable<GLSLInt>(name: "uIndex1")
-        let uIndex2 = Variable<GLSLInt>(name: "uIndex2")
         let projecteeIndex = Variable<GLSLInt>(name: "projecteeIndex")
         
         fragmentScope ✍ FixedGPUInstruction(code: stringFromLines([
             "gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
-            
-            
-            "int projecteeIndex = 100;",
+
             "if (texel.y > 1.0) {",
             "   if(texel.x < 0.0) {",
             "       if(texel.y - 1.0 > -texel.x) {",
@@ -91,112 +85,58 @@ extension DefaultPipelines {
             "   return;",
             "}",
             
-            // - -
             "if (projecteeIndex < 3) {",
             "   lowp float errY = -texel.y;",
             "   lowp float splasher = (texel.x - 0.5) / (texel.y - 0.5);",
             "   lowp float tX = errY * splasher;",
             "   texel = vec2(texel.x + tX, errY);",
-            "   gl_FragColor = texture2D(uCubeTextureCurrent, texel);",
             "} else {",
             "   lowp float errX = -texel.x;",
             "   lowp float splasher = (texel.y - 0.5) / (texel.x - 0.5);",
             "   lowp float tY = errX * splasher;",
             "   texel = vec2(errX, texel.y + tY);",
-            "   gl_FragColor = texture2D(uCubeTextureLeft, texel);",
+            "}"
+            ]), usedVariables: [GPUUniforms.CubeTextures.Current, GPUUniforms.CubeTextures.Left, projecteeIndex])
+        
+        
+        [0, 1, 0, 2]
+        fragmentScope ✍ FixedGPUInstruction(code: stringFromLines([
+            "if (projecteeIndex == 4) {",
+            "   transformationIndex = uIndex1;",
             "}",
             
             
-            "texture2D(uCubeTextureCurrent, vec2(0.0, 0.0));",
-            "texture2D(uCubeTextureLeft, vec2(0.0, 0.0));"
-            
-            ]), usedVariables: [GPUUniforms.CubeTextures.Current, GPUUniforms.CubeTextures.Left])
-//            ]), usedVariables: [wallIndex, texel, projecteeIndex])
-//        
-//        let texelTransformation = Variable<GLSLMat2>(name: "texelTransformation")
-//        
-//        
-//        var iii = 0
-//        let pros = [
-////            (2, 2),
-////            (10, 0),
-////            (1, 1),     // 2
-////            (10, 0),
-////            (10, 0),
-////            (2, 0),     // 5
-////            (10, 0),
-////            (10, 0),
-////            (3, 0),     // 8
-////            (10, 0),
-////            (10, 0),
-////            (4, 0),    // 11
-////            (0, 0)]
-//            (10, 0),
-//            (1, 0),
-//            (1, 0),     // 2
-//            (10, 0),
-//            (10, 0),
-//            (10, 0),//(2, 0),     // 5
-//            (10, 0),
-//            (10, 0),
-//            (10, 0),     // 8
-//            (10, 0),
-//            (10, 0),
-//            (10, 0),    // 11
-//            (10, 0)]
-//        for pro in pros {
-//            fragmentScope ✍ ConditionInstruction(bool: projecteeIndex .== Primitive(value: iii), successInstructions: [
-//                wallIndex ⬅ Primitive(value: pro.0),
-//                transformationIndex ⬅ Primitive(value: pro.1)
-//                ])
-//            iii += 1
-//        }
-//        // 2 5 8
-//        fragmentScope ✍ ConditionInstruction(bool: projecteeIndex .== Primitive(value: 100), successInstructions: [
-//            wallIndex ⬅ uIndex1,
-//            transformationIndex ⬅ uIndex2
-//            ])
-//        
-//        
-//        var i = 0
-//        let transforms = [
-//            GLKMatrix2(m: (1, 0, 0, 1)),
-//            GLKMatrix2(m: (-1, 0, 0, 1)),
-//            GLKMatrix2(m: (1, 0, 0, -1)),
-//            GLKMatrix2(m: (-1, 0, 0, -1)),
-//            GLKMatrix2(m: (0, 1, 1, 0)),
-//            GLKMatrix2(m: (0, -1, 1, 0)),
-//            GLKMatrix2(m: (0, 1, -1, 0)),
-//            GLKMatrix2(m: (0, -1, -1, 0))
-//        ]
-//        for transform in transforms {
-//            fragmentScope ✍ ConditionInstruction(bool: transformationIndex .== Primitive(value: i), successInstructions: [
-//                texelTransformation ⬅ Primitive(value: transform)
-//                ])
-//            i += 1
-//        }
-//        
-//        fragmentScope ✍ texel ⬅ texelTransformation * texel
-//        
-//        
-//        let texs = [GPUUniforms.CubeTextures.Current, GPUUniforms.CubeTextures.Top, GPUUniforms.CubeTextures.Left, GPUUniforms.CubeTextures.Bottom, GPUUniforms.CubeTextures.Right]
-//        var ii = 0
-//        for tex in texs {
-//            fragmentScope ✍ ConditionInstruction(bool: wallIndex .== Primitive(value: ii), successInstructions: [
-//                OpenGLDefaultVariables.glFragColor() ⬅ tex ☒ texel])
-//            ii += 1
-//        }
-//        
-//        fragmentScope ✍ ConditionInstruction(bool: wallIndex .== Primitive(value: 10), successInstructions: [
-//            OpenGLDefaultVariables.glFragColor() ⬅ Primitive(value: (r: 1.0, g: 0.0, b: 0.0, a: 1.0))])
-//        
-//        fragmentScope ✍ ConditionInstruction(bool: wallIndex .== Primitive(value: 11), successInstructions: [
-//            OpenGLDefaultVariables.glFragColor() ⬅ Primitive(value: (r: 0.0, g: 1.0, b: 0.0, a: 1.0))])
-//        fragmentScope ✍ ConditionInstruction(bool: wallIndex .== Primitive(value: 12), successInstructions: [
-//            OpenGLDefaultVariables.glFragColor() ⬅ Primitive(value: (r: 1.0, g: 1.0, b: 0.0, a: 1.0))])
-//        fragmentScope ✍ ConditionInstruction(bool: wallIndex .== Primitive(value: 13), successInstructions: [
-//            OpenGLDefaultVariables.glFragColor() ⬅ Primitive(value: (r: 0.5, g: 0.5, b: 0.0, a: 1.0))])
-//        
+            "if (transformationIndex == 1) {",
+            "   texel.x = -texel.x;",
+            "} else if (transformationIndex == 2) {",
+            "   texel.y = -texel.y;",
+            "} else if (transformationIndex == 3) {",
+            "   texel = -texel;",
+            "} else if (transformationIndex == 4) {",
+            "   texel.x = texel.y;",
+            "   texel.y = texel.x;",
+            "} else if (transformationIndex == 5) {",
+            "   texel.x = -texel.y;",
+            "   texel.y = texel.x;",
+            "} else if (transformationIndex == 6) {",
+            "   texel.x = texel.y;",
+            "   texel.y = -texel.x;",
+            "} else if (transformationIndex == 7) {",
+            "   texel.x = -texel.y;",
+            "   texel.y = -texel.x;",
+            "}"
+            ]), usedVariables: [texel, transformationIndex, GPUUniforms.index1])
+        
+        let samplers = [GPUUniforms.CubeTextures.Current,
+                        GPUUniforms.CubeTextures.Bottom,
+                        GPUUniforms.CubeTextures.Top,
+                        GPUUniforms.CubeTextures.Left,
+                        GPUUniforms.CubeTextures.Right]
+        for i in 0...samplers.count - 1 {
+            fragmentScope ✍ ConditionInstruction(bool: projecteeIndex .== Primitive(value: i), successInstructions: [
+                OpenGLDefaultVariables.glFragColor() ⬅ samplers[i] ☒ texel
+                ])
+        }
         
         let program = SmartPipelineProgram(vertexScope: vertexScope, fragmentScope: fragmentScope)
         NSLog("\n" + GLSLParser.scope(program.pipeline.vertexShader.function.scope!))
