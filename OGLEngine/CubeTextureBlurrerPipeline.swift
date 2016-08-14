985391521d8d159704c3e52ff3f9a3cb2aba8d63
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-08-14 15:14:54 +0200

    blur works but laggy

--- OGLEngine/CubeTextureBlurrerPipeline.swift
+++ OGLEngine/CubeTextureBlurrerPipeline.swift
@@ -28,9 +28,31 @@ extension DefaultPipelines {
         fragmentScope ⥥ GPUUniforms.CubeTextures.Right
         fragmentScope ⥥ GPUUniforms.sideTexturesTransformations
         
+        
         let cubeMapColorWithTexel = DefaultGPUFunction.cubeMapColorWithTexel()
         fragmentScope ↳ cubeMapColorWithTexel
         fragmentScope ✍ OpenGLDefaultVariables.glFragColor() ⬅ (cubeMapColorWithTexel .< [texel])
+        
+        let blurRadius: Float = 0.008
+        let samplesRadius = 1
+        let samplesInLine = 1 + samplesRadius * 2
+        let samplesCount = Int(pow(Float(samplesInLine), Float(2)))
+        let sampleDistance = blurRadius * 2.0 / Float(samplesInLine - 1)
+        
+        fragmentScope ✍ FixedGPUInstruction(code: stringFromLines([
+            "lowp float z = 0.0;",
+            "lowp vec4 blurredColor = vec4(0.0, 0.0, 0.0, 0.0);",
+            "for (int ix = -\(samplesRadius); ix <= \(samplesRadius); ix++) {",
+            "   for (int iy = -\(samplesRadius); iy <= \(samplesRadius); iy++) {",
+            "       lowp vec2 iTexel = vec2(texel.x + float(ix) * \(sampleDistance), texel.y + float(iy) * \(sampleDistance));",
+            "       lowp vec4 iColor = cubeMapColorWithTexel(iTexel);",
+            "       blurredColor = blurredColor + iColor;",
+            "   }",
+            "}",
+            "gl_FragColor = blurredColor / \(samplesCount).0;"
+            ]), usedVariables: [])
+        
+//        fragmentScope ✍ OpenGLDefaultVariables.glFragColor() ⬅ (cubeMapColorWithTexel .< [texel])
         
         let program = SmartPipelineProgram(vertexScope: vertexScope, fragmentScope: fragmentScope)
         NSLog("\n" + GLSLParser.scope(program.pipeline.vertexShader.function.scope!))
